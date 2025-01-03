eaa68282031e2adc87a617e69ef0a3812af0ebe1
Abdurahmon086 <1937ali047@gmail.com>
2025-01-04 01:11:03 +0500

    add new ShareButton component; update main title styles and colors; enhance index layout with new sections

---
+++ components/share/button.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+<template>
+    <button class="main-btn">
+        <slot />
+    </button>
+</template>
+
+<style scoped>
+.main-btn {
+    background-color: #71b2f7;
+    padding: 21px 38px;
+    font-size: 14px;
+    line-height: 16.1px;
+    color: white;
+    border-radius: 3px;
+    width: fit-content;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -17,18 +17,41 @@
                     <CardsBrand />
                 </li>
             </ul>
-            <ul class="grid grid-cols-3 gap-6 my-10">
+        </div>
+    </div>
+    <section class="mt-12">
+        <div class="container">
+            <div class="rounded-xl bg-gradient-to-r from-[#3593F3] to-[#0C74DF] text-white">
+                <div class="bg-[url('/images/bg1.png')] bg-no-repeat bg-right pt-5 pb-8 px-10">
+                    <h4 class="text-[28px] leading-8 font-medium">Не можете определится с выбором?</h4>
+                    <p class="text-[15px] leading-5 max-w-[383px] mt-1 mb-3">
+                        Оставьте заявку, и наши менеджеры проконсультируют вас!
+                    </p>
+                    <ShareButton>Оставить заявку</ShareButton>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mt-24">
+        <div class="container">
+            <h3 class="main-title">Новости</h3>
+            <ul class="grid grid-cols-3 gap-6">
                 <li v-for="(item, i) in 3" :key="i">
                     <CardsNew />
                 </li>
             </ul>
-            <ul class="grid grid-cols-3 gap-6 my-10">
+        </div>
+    </section>
+    <section class="my-24">
+        <div class="container">
+            <h3 class="main-title">Отзывы</h3>
+            <ul class="grid grid-cols-3 gap-6">
                 <li v-for="(item, i) in 3" :key="i">
                     <CardsReview />
                 </li>
             </ul>
         </div>
-    </div>
+    </section>
 </template>
 
 <style></style>
