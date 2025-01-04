38af9611e9e579d76082bf8f0adaa242c0249c8b
Abdurahmon086 <1937ali047@gmail.com>
2025-01-04 13:21:58 +0500

    add loading component and enhance data handling in index.vue for improved user experience

---
+++ components/share/loading.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="animate-spin h-8 w-8 border-4 border-blue-500 border-t-transparent rounded-full"></div>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { aboutUs } from "~/constants";
 
-const { data } = await useAsyncData(
+const { data, error, status } = await useAsyncData(
     "home",
     async () => {
         const [cars, brands, news] = await Promise.all([
@@ -17,114 +17,117 @@ const { data } = await useAsyncData(
 );
 </script>
 <template>
-    <section class="mt-8">
-        <SwipersHero :data="data?.cars.slice(3, 6)" />
-    </section>
+    <template v-if="status === 'pending'"><ShareLoading /></template>
+    <template v-else-if="data && status === 'success'">
+        <section class="mt-8">
+            <SwipersHero :data="data?.cars.slice(3, 6)" />
+        </section>
 
-    <!-- Подбор авто section -->
-    <section class="mt-24">
-        <SectionsFilterAvto />
-    </section>
+        <!-- Подбор авто section -->
+        <section class="mt-24">
+            <SectionsFilterAvto />
+        </section>
 
-    <section class="mt-24">
-        <div class="container">
-            <h3 class="main-title">АВТОМОБИЛЬНЫЙ КАТАЛОГ</h3>
-            <ul class="grid grid-cols-3 gap-6">
-                <li v-for="item in data?.cars?.slice(0, 6)" :key="item?.id">
-                    <CardsMainCar :data="item" />
-                </li>
-            </ul>
-            <NuxtLink to="/" class="flex-center mt-4 justify-end">
-                <p class="border-b border-grayer text-sm leading-[18px]">Перейти в каталог</p>
-                <Icon name="uil:arrow-right" class="ml-1 text-xl" />
-            </NuxtLink>
-        </div>
-    </section>
+        <section class="mt-24">
+            <div class="container">
+                <h3 class="main-title">АВТОМОБИЛЬНЫЙ КАТАЛОГ</h3>
+                <ul class="grid grid-cols-3 gap-6">
+                    <li v-for="item in data?.cars?.slice(0, 6)" :key="item?.id">
+                        <CardsMainCar :data="item" />
+                    </li>
+                </ul>
+                <NuxtLink to="/" class="flex-center mt-4 justify-end">
+                    <p class="border-b border-grayer text-sm leading-[18px]">Перейти в каталог</p>
+                    <Icon name="uil:arrow-right" class="ml-1 text-xl" />
+                </NuxtLink>
+            </div>
+        </section>
 
-    <!-- Почему мы? section -->
-    <section class="mt-24">
-        <div class="container">
-            <h3 class="main-title">Почему мы?</h3>
-            <ul class="grid grid-cols-4 gap-6">
-                <li v-for="item in aboutUs" :key="item?.id">
-                    <CardsAboute :data="item" />
-                </li>
-            </ul>
-        </div>
-    </section>
+        <!-- Почему мы? section -->
+        <section class="mt-24">
+            <div class="container">
+                <h3 class="main-title">Почему мы?</h3>
+                <ul class="grid grid-cols-4 gap-6">
+                    <li v-for="item in aboutUs" :key="item?.id">
+                        <CardsAboute :data="item" />
+                    </li>
+                </ul>
+            </div>
+        </section>
 
-    <!-- О нашей компании section -->
-    <section class="mt-28">
-        <div class="container">
-            <div class="bg-[#F6F6F6] rounded-lg flex justify-between h-[390px]">
-                <div
-                    class="bg-[url('/images/person-bg.png')] bg-no-repeat w-1/3 h-min ml-10"
-                    style="background-position: 10px -8px"
-                >
-                    <img src="/images/person.png" alt="person image" class="h-[442px] -translate-y-[52px]" />
-                </div>
-                <div class="kompny-us pt-[53px] pr-4">
-                    <h3 class="text-[30px] leading-[36px] font-medium">О нашей компании</h3>
-                    <p class="mt-2 mb-5 max-w-[623px] text-[#989898]">
-                        Мы имеем огромный опыт работы с автомобильным рынком Кореи и тщательно отбираем автомобили для
-                        наших клиентов, учитывая их потребности и бюджет. Мы работаем только с надежными поставщиками и
-                        перевозчиками, чтобы обеспечить безопасность и надежность доставки.
-                    </p>
-                    <ShareButton>Связаться с нами</ShareButton>
+        <!-- О нашей компании section -->
+        <section class="mt-28">
+            <div class="container">
+                <div class="bg-[#F6F6F6] rounded-lg flex justify-between h-[390px]">
+                    <div
+                        class="bg-[url('/images/person-bg.png')] bg-no-repeat w-1/3 h-min ml-10"
+                        style="background-position: 10px -8px"
+                    >
+                        <img src="/images/person.png" alt="person image" class="h-[442px] -translate-y-[52px]" />
+                    </div>
+                    <div class="kompny-us pt-[53px] pr-4">
+                        <h3 class="text-[30px] leading-[36px] font-medium">О нашей компании</h3>
+                        <p class="mt-2 mb-5 max-w-[623px] text-[#989898]">
+                            Мы имеем огромный опыт работы с автомобильным рынком Кореи и тщательно отбираем автомобили
+                            для наших клиентов, учитывая их потребности и бюджет. Мы работаем только с надежными
+                            поставщиками и перевозчиками, чтобы обеспечить безопасность и надежность доставки.
+                        </p>
+                        <ShareButton>Связаться с нами</ShareButton>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!-- Brands section-->
-    <section class="mt-24">
-        <div class="container">
-            <ul class="grid grid-cols-6 my-10">
-                <li v-for="item in data?.brands" :key="item?.id">
-                    <CardsBrand :data="item" />
-                </li>
-            </ul>
-        </div>
-    </section>
+        <!-- Brands section-->
+        <section class="mt-24">
+            <div class="container">
+                <ul class="grid grid-cols-6 my-10">
+                    <li v-for="item in data?.brands" :key="item?.id">
+                        <CardsBrand :data="item" />
+                    </li>
+                </ul>
+            </div>
+        </section>
 
-    <!-- выбором section-->
-    <section class="mt-12">
-        <div class="container">
-            <div class="rounded-xl bg-gradient-to-r from-[#3593F3] to-[#0C74DF] text-white">
-                <div class="bg-[url('/images/bg1.png')] bg-no-repeat bg-right pt-5 pb-8 px-10">
-                    <h4 class="text-[28px] leading-8 font-medium">Не можете определится с выбором?</h4>
-                    <p class="text-[15px] leading-5 max-w-[383px] mt-1 mb-4">
-                        Оставьте заявку, и наши менеджеры проконсультируют вас!
-                    </p>
-                    <ShareButton color="bg-[#71B2F7]">Оставить заявку</ShareButton>
+        <!-- выбором section-->
+        <section class="mt-12">
+            <div class="container">
+                <div class="rounded-xl bg-gradient-to-r from-[#3593F3] to-[#0C74DF] text-white">
+                    <div class="bg-[url('/images/bg1.png')] bg-no-repeat bg-right pt-5 pb-8 px-10">
+                        <h4 class="text-[28px] leading-8 font-medium">Не можете определится с выбором?</h4>
+                        <p class="text-[15px] leading-5 max-w-[383px] mt-1 mb-4">
+                            Оставьте заявку, и наши менеджеры проконсультируют вас!
+                        </p>
+                        <ShareButton color="bg-[#71B2F7]">Оставить заявку</ShareButton>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!-- Новости section-->
-    <section class="mt-24">
-        <div class="container">
-            <h3 class="main-title">Новости</h3>
-            <ul class="grid grid-cols-3 gap-6">
-                <li v-for="item in data?.news?.slice(0, 3)" :key="item?.id">
-                    <CardsNew :data="item" />
-                </li>
-            </ul>
-        </div>
-    </section>
+        <!-- Новости section-->
+        <section class="mt-24">
+            <div class="container">
+                <h3 class="main-title">Новости</h3>
+                <ul class="grid grid-cols-3 gap-6">
+                    <li v-for="item in data?.news?.slice(0, 3)" :key="item?.id">
+                        <CardsNew :data="item" />
+                    </li>
+                </ul>
+            </div>
+        </section>
 
-    <!-- Отзывы section-->
-    <section class="my-24">
-        <div class="container">
-            <h3 class="main-title">Отзывы</h3>
-            <ul class="grid grid-cols-3 gap-6">
-                <li v-for="(item, i) in 3" :key="i">
-                    <CardsReview />
-                </li>
-            </ul>
-        </div>
-    </section>
+        <!-- Отзывы section-->
+        <section class="my-24">
+            <div class="container">
+                <h3 class="main-title">Отзывы</h3>
+                <ul class="grid grid-cols-3 gap-6">
+                    <li v-for="(item, i) in 3" :key="i">
+                        <CardsReview />
+                    </li>
+                </ul>
+            </div></section
+    ></template>
+    <template v-else>{{ error }}</template>
 </template>
 
 <style>
