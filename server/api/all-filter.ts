550d6e6048277e0b906f33e1872d7acb8f34975c
Abdurahmon086 <1937ali047@gmail.com>
2025-02-07 09:25:54 +0500

    refactor all-filter API handler; remove console log for cleaner code

--- server/api/all-filter.ts
+++ server/api/all-filter.ts
@@ -4,7 +4,6 @@ export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig();
     const url = config.public.API_BASE_URL;
     const body = await readBody(event);
-    console.log(body);
 
     const res = await $fetch<Car[]>(`${url}/all-filter`, {
         method: "POST",
